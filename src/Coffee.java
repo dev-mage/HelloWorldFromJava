1ae175aa344881c112d0ba808186b920344b8959
dev-mage <hxxlxx0@gmail.com>
2022-09-19 19:01:07 +0900

    2022-09-19 객체지향 프로그래밍4 - 캡슐화

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,15 +1,24 @@
-public class Coffee extends Beverage{
-    int extraShot;
-    public Coffee(String name, int price, int extraShot) {
-        super(name, price);
+public class Coffee{
+    private int price;
+    private int extraShot;
+    public int calculatePrice() {
+        return this.getPrice() + (this.getExtraShot() * 500);
+    }
+    public void setPrice(int price) {
+        if(price < 3000) price = 3000;
+        this.price = price;
+    }
+    public int getPrice() { return price; }
+    public void setExtraShot(int extraShot) {
+        if(extraShot < 0) extraShot = 0;
         this.extraShot = extraShot;
     }
-    int calculatePrice() {
-        return price + (extraShot * 500);
-    }
+    public int getExtraShot() { return extraShot; }
 
     public static void main(String[] args) {
-        Coffee coffee = new Coffee("아메리카노", 3000, 2);
-        System.out.println( coffee.name + ": " + coffee.calculatePrice() + "원");
+        Coffee coffee = new Coffee();
+        coffee.setPrice(3000);
+        coffee.setExtraShot(2);
+        System.out.println(coffee.calculatePrice());
     }
 }
