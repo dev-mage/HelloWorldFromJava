ed059aa054e59c9c436c0b2ab49ca6336db16504
dev-mage <hxxlxx0@gmail.com>
2022-10-08 19:27:56 +0900

    2022-10-08 예외

---
+++ src/UserException.java
@@ -0,0 +1,3 @@
+public class UserException extends RuntimeException {
+
+}
---
+++ src/file/FileGenerator.java
@@ -0,0 +1,29 @@
+package file;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileGenerator {
+    static File generateFile(String fileName) throws Exception {
+        if(fileName == null || fileName.equals("")) {
+            throw new Exception("파일 이름이 유효하지 않습니다.");
+        }
+        File file = new File("./src/file/files/", fileName);
+        file.createNewFile();
+        return file;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("파일 이름을 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        try {
+            File file = generateFile(fileName);
+            System.out.println(file.getName() + " 파일이 성공적으로 생성되었습니다.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " 다시 실행해 주시기 바랍니다.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
